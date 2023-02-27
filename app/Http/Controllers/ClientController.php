a79f3468de0a0aabb9feaa9fd41d1c5460eefe06
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-02-27 16:39:10 +0100

    Return the view for creating a new client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -20,9 +20,9 @@ class ClientController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(): Response
+    public function create(): View
     {
-        //
+        return view('clients.create'); 
     }
 
     /**
