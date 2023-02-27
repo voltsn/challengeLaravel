9b19e087aca0659763d5173e5412b2597354a334
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-02-27 14:44:19 +0100

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\InvoiceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clients', [ClientController::class, 'index']); 
+
+Route::get('/clients/edit/{client:id}', [ClientController::class, 'edit']);
+
+Route::patch('/clients/edit/{client:id}', [ClientController::class, 'update']);
+
+Route::get('/invoices', [InvoiceController::class, 'index']);
+
+Route::get('/invoices/edit/{invoice:id}', [InvoiceController::class, 'edit']);
+
+Route::patch('/invoices/edit/{invoice:id}', [InvoiceController::class, 'update']);
