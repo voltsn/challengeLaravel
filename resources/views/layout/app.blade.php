62e1ee981e383c09f8a8926b5c529dad7efa764c
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-01 12:22:12 +0100

    Link css and add styling

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-full" lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>@yield('title')</title>
+  @vite('resources/css/app.css')
 </head>
-  <body>
-    <header>
-      <h1>Gestionnaire clients - factures</h1>
+  <body class="h-full">
+    <header class="bg-red-800 text-white items-center justify-center flex flex-col h-20 md:h-40 px-2 py-2">
+      <h1 class="text-lg sm:text-2xl md:text-3xl ">Gestionnaire clients - factures</h1>
     </header>
     <main>
       @yield('content')
