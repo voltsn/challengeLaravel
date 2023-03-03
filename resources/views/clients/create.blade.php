67178326f3aaa61a916f2e1fa5514ad6d3ebf288
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-03 15:26:00 +0100

    Improve error handling and add a message in case of success

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -5,40 +5,53 @@
 @section('content')
     <div class="flex flex-col justify-center m-auto my-8 w-5/6 max-w-3xl">
         <h2 class="text-3xl bg-slate-200 p-3 mb-8">Ajouter un client</h2>
+        @if (session('status'))
+            <p class="text-center mb-5 bg-green-400 py-5" >{{ session('status') }}</p>
+        @endif
         <form action="/clients/create" method="POST">
             @csrf
             <div>
-                <input type="text" name="name" placeholder="Nom du client">
                 @if ($errors->has('name'))
-                    <p>{{ $errors->first('name') }}
+                    <input class="border-red-400" type="text" name="name" placeholder="Nom du client">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('name') }}
+                @else
+                    <input  type="text" name="name" placeholder="Nom du client">
                 @endif
             </div>
 
             <div>
-                <input type="number" name="phoneNumber" placeholder="Numéro de téléphone">
                 @if ($errors->has("phoneNumber"))
-                    <p>{{ $errors->first('phoneNumber') }}</p>
+                    <input class="border-red-400" type="number" name="phoneNumber" placeholder="Numéro de téléphone">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('phoneNumber') }}</p>
+                @else
+                     <input type="number" name="phoneNumber" placeholder="Numéro de téléphone">
                 @endif
             </div>
 
             <div>
-                <input type="email" name="email" placeholder="Email">
                 @if ($errors->has('email'))
-                    <p>{{ $errors->first('email') }}</p>
+                    <input class="border-red-400" type="email" name="email" placeholder="Email">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('email') }}</p>
+                @else 
+                    <input type="email" name="email" placeholder="Email">
                 @endif
             </div>
 
             <div>
-                <input type="text" name="address" placeholder="Address">
                 @if ($errors->has('address'))
-                    <p>{{ $errors->first('address')</p>
+                    <input class="border-red-400" type="text" name="address" placeholder="Address">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('address') }}</p>
+                @else
+                    <input type="text" name="address" placeholder="Address">
                 @endif
             </div>
 
             <div>
-                <input type="text" name="tva" placeholder="Numéro de tva">
                 @if ($errors->has('tva'))
-                    <p>{{ $errors->first('tva')</p>
+                    <input class="border-red-400" type="text" name="tva" placeholder="Numéro de tva">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('tva') }}</p>
+                @else
+                    <input type="text" name="tva" placeholder="Numéro de tva">
                 @endif
             </div>
 
