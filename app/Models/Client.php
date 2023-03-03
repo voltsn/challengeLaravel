cd3cd6a0088014a3a55361739f64d4312f1079da
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-03 15:34:39 +0100

    Link Client model to the database and add fillable

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    protected $table = 'clients';
+    protected $fillable = ['name', 'phoneNumber', 'email', 'address', 'tva'];
 }
