bffd03e7e2ec0cb914a6af5cdb01535c8323a10f
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-03 15:20:06 +0100

    Create migrations

---
+++ database/migrations/2023_03_01_113105_create_clients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('phoneNumber');
+            $table->string('email');
+            $table->string('address');
+            $table->string('tva');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
