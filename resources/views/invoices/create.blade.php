912a446412306d47b6750fae1ef31c2789e912d2
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-05 16:23:41 +0100

    Improve the error messages

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -5,47 +5,62 @@
 @section('content')
     <div class="flex flex-col justify-center m-auto my-8 w-5/6 max-w-3xl">
         <h2  class="text-3xl bg-slate-200 p-3 mb-8">Créer une facture</h2>
+        @if (session('status'))
+            <p class=" text-center mb-4 bg-green-300 py-5">{{ session('status') }}</p>
+        @endif
         <form action="/invoices/create" method="POST">
             @csrf
             <div>
-                <input type="text" name="client" placeholder="Client">
                 @if ($errors->has('client'))
-                    <p>{{ $errors->first('client') }}</p>
+                    <input class="border-red-400" type="text" name="client" placeholder="Client">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('client') }}</p>
+                @else
+                    <input type="text" name="client" placeholder="Client">
                 @endif
             </div>
 
             <div>
-                <input type="text" name="ref" placeholder="Référence">
                 @if ($errors->has('ref'))
-                    <p>{{ $errors->first('ref') }}
+                    <input class="border-red-400" type="text" name="ref" placeholder="Référence">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('ref') }}</p>
+                @else
+                    <input type="text" name="ref" placeholder="Référence">
                 @endif
             </div>
 
             <div>
-                <input type="title" name="title" placeholder="Titre">
-                @if ($errors->has("phoneNumber"))
-                    <p>{{ $errors->first('phoneNumber') }}</p>
+                @if ($errors->has('title'))
+                    <input class="border-red-400" type="title" name="title" placeholder="Titre">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('title') }}</p>
+                @else 
+                     <input type="title" name="title" placeholder="Titre">
                 @endif
             </div>
 
             <div>
-                <input type="text" name="goal" placeholder="Raison">
                 @if ($errors->has('goal'))
-                    <p>{{ $errors->first('goal') }}</p>
+                    <input class="border-red-400" type="text" name="goal" placeholder="Raison">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('goal') }}</p>
+                @else
+                    <input type="text" name="goal" placeholder="Raison">
                 @endif
             </div>
 
             <div>
-                <input type="number" name="tva" placeholder="tva">
                 @if ($errors->has('tva'))
-                    <p>{{ $errors->first('tva') }}</p>
+                    <input class="border-red-400" type="number" name="tva" placeholder="tva">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('tva') }}</p>
+                @else 
+                    <input type="number" name="tva" placeholder="tva">
                 @endif
             </div>
 
             <div>
-                <input type="number" name="amount" placeholder="Montant">
-                @if ($errors->has('email'))
-                    <p>{{ $errors->first('email') }}</p>
+                @if ($errors->has('amount'))
+                    <input class="border-red-400" type="number" name="amount" placeholder="Montant">
+                    <p class="text-sm text-red-500 p-2">{{ $errors->first('amount') }}</p>
+                @else
+                    <input type="number" name="amount" placeholder="Montant">
                 @endif
             </div>
 
