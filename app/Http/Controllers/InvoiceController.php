a8e700563b1a6aa86701b4f2cdefe7eaeeac5ac9
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-03-05 16:19:41 +0100

    Handle creating new invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use App\Models\Client;
+use App\Http\Requests\InvoicePostRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class InvoiceController extends Controller
 {
@@ -28,9 +32,29 @@ class InvoiceController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request): RedirectResponse
+    public function store(InvoicePostRequest $request): RedirectResponse
     {
-        //
+        $request->validated();
+
+        $client_id = NULL;
+
+        try{
+          $client = Client::where('name', $request['client'])->firstOrFail();
+          $client_id =  $client->id;
+        } catch(ModelNotFoundException $e){
+            return redirect("/invoices/create")->with('status', 'Le client mentionner n\'existe pas...');
+        }
+
+        Invoice::create([
+            'client_id' => $client_id,
+            'ref' => $request['ref'],
+            'title' => $request['title'],
+            'tva' => $request['tva'],
+            'price' => $request['amount'],
+            'goal' => $request['goal'],
+        ]);
+
+        return redirect("/invoices/create")->with('status', 'Le Facture été enregistrer avec succes');
     }
 
     /**
